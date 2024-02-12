8d6b1fd164643ba78fe82d98f0b7dbb480b1c182
banuaggun <banuaggun@outlook.com>
2024-02-12 15:26:44 +0300

    The search field has been created and made operational.

---
+++ src/components/Searchbox.vue
@@ -0,0 +1,27 @@
+<template>
+  <section id="search-box">
+    <form action="" @submit.prevent="handleSubmit()">
+      <input type="text" placeholder="Enter a word" v-model="searchText"/>
+      <button type="submit">Search</button>
+    </form>
+  </section>
+</template>
+<script>
+export default {
+  name:'search-box',
+  data(){
+    return{
+      searchText:''
+    }
+  },
+  methods:{
+    handleSubmit(){
+      console.log(this.searchText)
+      this.searchText=''
+    }
+  }
+}
+</script>
+<style>
+  
+</style>

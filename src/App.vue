f63e0a4898b65135ba45bd05c0d16c7cfceef52c
banuaggun <banuaggun@outlook.com>
2024-02-14 00:46:10 +0300

    Input field is recreating with Composition API

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <script setup>
 import Header from './components/Header.vue'
 import SearchBox from './components/Searchbox.vue'
+import {ref} from 'vue'
+
+const searchText = ref()
+
 </script>
 
 <template>
   <div id="wrapper">
     <Header/>
     <main>
-      <SearchBox />
+      <SearchBox v-model:searchText="searchtext"/>
+
     </main>
   </div>
 </template>
--- src/components/Searchbox.vue
+++ src/components/Searchbox.vue
@@ -1,25 +1,19 @@
 <template>
   <section id="search-box">
     <form action="" @submit.prevent="handleSubmit()">
-      <input type="text" placeholder="Enter a word" v-model="searchText"/>
+      <input type="text" :value="props.searchText" class="search-box-input" :placeholder="props.placeholder" @input="searchHandler"/>
       <button type="submit">Search</button>
     </form>
   </section>
 </template>
-<script>
-export default {
-  name:'search-box',
-  data(){
-    return{
-      searchText:''
-    }
-  },
-  methods:{
-    handleSubmit(){
-      console.log(this.searchText)
-      this.searchText=''
-    }
-  }
+<script setup>
+const props=defineProps(["searchText", "placeholder"])
+
+const emits=defineEmits(["update:searchText"])
+
+const searchHandler = (event) => {
+  emits("update:searchText", event.target.value)
+  console.log(emits);
 }
 </script>
 <style>

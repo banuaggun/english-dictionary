31b3e278b63ffa7cd8fc9f23b82e8c4ff099678d
banuaggun <banuaggun@outlook.com>
2024-02-13 22:01:50 +0300

    Header has been turned into a component

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,13 @@
 <script setup>
+import Header from './components/Header.vue'
 import SearchBox from './components/Searchbox.vue'
 </script>
 
 <template>
   <div id="wrapper">
-    <header>
-      <img src="./assets/vue.svg" alt="dictionary logo" />
-      
-    </header>
+    <Header/>
     <main>
-      <h1>ENGLISH DICTIONARY</h1>
       <SearchBox />
     </main>
   </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <img src="../assets/vue.svg" alt="dictionary logo" />
+    <h2>English dictionary</h2>
+  </header>
+</template>
+
+<style scoped>
+header {
+  width:100%;
+  height:80px;
+  display:flex;
+  align-items:center;
+  border:1px solid green;
+}
+
+img{
+  margin-right:6px;
+}
+
+h2{
+  text-transform: uppercase;
+}
+</style>

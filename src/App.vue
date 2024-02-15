aae9b55c2adbbc4a3661676d1bd8d576df76590f
banuaggun <banuaggun@outlook.com>
2024-02-15 15:33:42 +0300

    Tested whether the API works or not

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <script setup>
 import Header from './components/Header.vue'
 import SearchBox from './components/Searchbox.vue'
+import axios from 'axios'
 import {ref} from 'vue'
 
 const searchText = ref()
+
+const screenOutput = ref()
+
+async function getData(word){
+  const result = await axios(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`).then(response => console.log(response.data[0].meanings[0].definitions[0].definition))
+
+  return result
+}
+
+getData("color")
 
 </script>
 
@@ -11,8 +22,9 @@ const searchText = ref()
   <div id="wrapper">
     <Header/>
     <main>
-      <SearchBox v-model:searchText="searchtext"/>
-
+      <SearchBox v-model:searchText="searchText" placeholder="Enter a Word" />
+      <button @click="clickHandler">Search</button>
+      <p>{{screenOutput}}</p>
     </main>
   </div>
 </template>

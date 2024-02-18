a542e9fc802fe43d1a186fed104251a9ecfc733d
banuaggun <banuaggun@outlook.com>
2024-02-19 00:36:20 +0300

    The data was displayed on the screen in the newly layout

---
+++ src/components/SearchItems.vue
@@ -0,0 +1,28 @@
+<script setup>
+const props = defineProps({
+  searchData:{
+    required:true,
+  },
+});
+</script>
+<template>
+  <div>
+    <h1>{{searchData[0].word}}</h1>
+  </div>
+  <div v-for="meaning in searchData[0].meanings"
+  :key="meaning">
+  {{meaning.partOfSpeech}}
+   <ul role="list">
+          <li
+            v-for="definitionData in meaning.definitions"
+            :key="definitionData"
+          >
+            {{ definitionData.definition }}
+
+            <span v-if="definitionData.example">
+              Example: "{{ definitionData.example }}"
+            </span>
+          </li>
+        </ul>
+    </div>
+</template>
--- src/components/Searchbox.vue
+++ src/components/Searchbox.vue
@@ -1,20 +1,33 @@
 <script setup>
 import {ref} from 'vue'
 import axios from 'axios'
+import SearchItems from './SearchItems.vue'
 
 const searchRequests= ref("")
 
 const searchData = ref(null)
 
-const url=ref("https://api.dictionaryapi.dev/api/v2/entries/en/")
+const errorState = ref(false)
+const loadState = ref(false)
+
+const base_url=ref("https://api.dictionaryapi.dev/api/v2/entries/en/");
 
 const handleData = () => {
+  loadState.value = true;
   axios({
     method:"get",
-    url:`${url.value}${searchRequests.value}`,
+    url:`${base_url.value}${searchRequests.value}`,
     responseType:"json",
-  })
-}
+  }).then(function(response){
+    searchData.value = response.data;
+    loadState.value = false;
+  }).catch(function(error){
+    if(error){
+      errorState.value = true;
+    }
+  });
+  errorState.value = false;
+};
 
 </script>
 
@@ -22,11 +35,22 @@ const handleData = () => {
   <section>
     <form @submit.prevent="handleData">
       <label for="search-input">
-        <input type="text" name="text" placeholder="Search a word" v-model="searchRequests"/>
+        <input type="text" name="text" id="search-text" placeholder="Search a word" v-model="searchRequests"/>
         <span>
           <i class="fa-solid fa-magnifying-glass | text-base text-primaary_clr"></i>
         </span>
       </label>
     </form>
+    <article v-if="errorState">
+      <span>Image will come for 404 page</span>
+      <p>
+        We're sorry, we're not able to retrieve this information at the moment,
+        please try back later.
+      </p>
+    </article>
+    <article v-else-if="searchData">
+      <div v-if="loadState"> loading area will be created</div>
+      <SearchItems :search-data="searchData" />
+    </article>
   </section>
 </template>

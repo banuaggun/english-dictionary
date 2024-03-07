1b17e69a529197117c5e97a0baa69c98d4048c68
banuaggun <banuaggun@outlook.com>
2024-03-07 16:49:30 +0300

    Additional features of words were displayed on the screen.

--- src/components/SearchItems.vue
+++ src/components/SearchItems.vue
@@ -31,74 +31,107 @@ const removeAudio = () => {
 };
 </script>
 <template>
-  <!-- phonetic text and audio phonetics area -->
-  <div class="word-phonetic-audio">
+  <div class="page-area">
+    <!-- phonetic text and audio phonetics area -->
+    <div class="word-phonetic-audio">
 
-    <div class="word-phonetic">
-      <!-- word phonetic area -->
+      <div class="word-phonetic">
+        <!-- word phonetic area -->
 
-      <div class="word">
-        <h1>{{ searchData[0].word }}</h1>
-        <!-- word area -->
+        <div class="word">
+          <h1>{{ searchData[0].word }}</h1>
+          <!-- word area -->
+        </div>
+
+        <div class="phonetic">
+          <p v-if="searchData[0].phonetic || searchData[0].word">
+            {{ searchData[0].phonetic || searchData[0].word }}
+            <!-- phonetic text area -->
+          </p>
+        </div>
+        
       </div>
-
-      <div class="phonetic">
-        <p v-if="searchData[0].phonetic || searchData[0].word">
-          {{ searchData[0].phonetic || searchData[0].word }}
-          <!-- phonetic text area -->
-        </p>
-      </div>
-      
-    </div>
     
-    <div class="audio">
-      <div v-if="searchData[0].phonetics.length">
-        <audio @ended="removeAudio"
-            :src="
-                searchData[0].phonetics[0].audio ||
-                searchData[0].phonetics[1].audio ||
-                searchData[0].phonetics[2].audio
-            "
-        ></audio>
+      <div class="audio">
+        <div v-if="searchData[0].phonetics.length">
+          <audio @ended="removeAudio"
+              :src="
+                  searchData[0].phonetics[0].audio ||
+                  searchData[0].phonetics[1].audio ||
+                  searchData[0].phonetics[2].audio
+              "
+          ></audio>
+        </div>
+        <button v-if="searchData[0].phonetics.length" type="button" @click="playPauseAudio">
+          <i
+            id="audio-icon"
+            class="fa-solid fa-play | text-[1.25rem] text-primary_clr md:text-[1.5rem]"
+          ></i>
+        </button>
       </div>
-      <button v-if="searchData[0].phonetics.length" type="button" @click="playPauseAudio">
-        <i
-          id="audio-icon"
-          class="fa-solid fa-play | text-[1.25rem] text-primary_clr md:text-[1.5rem]"
-        ></i>
-      </button>
     </div>
-  </div>
 
-  <div 
-    v-for="meaning in searchData[0].meanings" 
-    :key="meaning"
-    class="meaning"
-  >
-    <h5 class="meaning-type">
-      {{ meaning.partOfSpeech }}
-    </h5>
-    <ul role="list">
-      <li
-        v-for="definitionData in meaning.definitions"
-        :key="definitionData"
-      >
-        <p> 
-          {{ definitionData.definition }} 
-        </p>
-        <span v-if="definitionData.example">
-          <b><i>Example:</i></b> "{{ definitionData.example }}"
-        </span>
-      </li>
-    </ul>
+    <div 
+      v-for="meaning in searchData[0].meanings" 
+      :key="meaning"
+      class="meaning"
+    >
+      <h5 class="meaning-type">
+        {{ meaning.partOfSpeech }}
+      </h5>
+      <ul role="list">
+        <li
+          v-for="definitionData in meaning.definitions"
+          :key="definitionData"
+        >
+          <p> 
+            {{ definitionData.definition }} 
+          </p>
+          <span v-if="definitionData.example">
+            <b><i>Example:</i></b> "{{ definitionData.example }}"
+          </span>
+        </li>
+        <li class="synonyms"> 
+          <span v-if="meaning.synonyms[0]">
+            <b><i>Synonyms:</i></b> {{meaning.synonyms[0]}}
+          </span>
+        </li>
+        <li class="antonyms"> 
+          <span v-if="meaning.antonyms[0]">
+            <b><i>Antonyms:</i></b> {{meaning.antonyms[0]}}
+          </span>
+        </li>
+        <li class="source"> 
+          <span>
+            <b><i>Source:</i></b> <a :href="searchData[0].sourceUrls[0]" target="_blank">
+                {{searchData[0].sourceUrls[0]}}  <i class="fa-solid fa-arrow-up-right-from-square | text-[0.8125rem] ml-1"></i>
+          </a>
+          </span>
+        </li>
+      </ul>
+    </div>
+
   </div>
 </template>
 
 <style scoped>
-h1 {
+.page-area{
+  border:1px solid #dcdcdc;
+  width:100%;
+  min-height:100vh;
+  margin:20px auto;
+  padding:0 20px;
+  border-radius: 12px;
+  box-shadow:-1px 2px 3px #dcdcdc;
+}
+.synonyms,
+.antonyms{
   text-transform: capitalize;
 }
-
+h1,
+.meaning-type {
+  text-transform: capitalize;
+}
 .phonetic {
   font-weight: 500;
 }

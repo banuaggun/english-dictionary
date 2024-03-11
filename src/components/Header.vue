5a5750a2e86edb8bd2227380c0fcdd3f492c67f9
banuaggun <banuaggun@outlook.com>
2024-03-11 14:40:39 +0300

    Header area is being created

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,24 +1,102 @@
 <template>
   <header>
-    <img src="../assets/vue.svg" alt="dictionary logo" />
-    <h2>English dictionary</h2>
+    <div class="web-navbar">
+      <div class="row-1">
+        <img src="../assets/icons/logo.svg" alt="dictionary logo" />
+      <h2>English dictionary</h2>
+    </div>
+    <div class="social-media">
+      <div class="social-media-portfolio">
+        <a href="https://github.com/banuaggun" target="_blank">
+          <span>GitHub</span>
+        </a>
+        <a href="https://portfolio-banuaggun.vercel.app/" target="_blank">
+          <span>Portfolio</span>
+        </a>
+      </div>
+    </div>
+    </div>
+     <div class="mobile-navbar">
+      <div class="mobile-navbar-row-1">
+        <img src="../assets/icons/logo.svg" alt="dictionary logo" />
+      <h2>English dictionary</h2>
+    </div>
+    <div class="mobile-navbar-row-2">
+      <div class="social-media-portfolio">
+        <a href="https://github.com/banuaggun" target="_blank">
+          GitHub
+        </a>
+        <a href="https://portfolio-banuaggun.vercel.app/" target="_blank">
+          Portfolio
+        </a>
+      </div>
+    </div>
+    </div>
   </header>
 </template>
 
 <style scoped>
-header {
-  width:100%;
-  height:80px;
-  display:flex;
-  align-items:center;
+header .mobile-navbar {
   border:1px solid green;
+  display:flex;
+  flex-wrap: wrap;
+  align-items: center;
+  margin:20px auto;
+  height:70px;
+}
+
+.mobile-navbar-row-1{
+  width:100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border:1px solid red;
+}
+.mobile-navbar-row-2 {
+  display:flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  border:1px solid green;
+  width: 100%;
+  padding-right:40px;
+}
+
+.mobile-navbar-row-2 a:nth-child(1){
+  margin-right:20px;
+}
+
+.mobile-navbar-row-2 a:hover,
+.mobile-navbar-row-2 a.active{
+  border-bottom:3px solid var(--bg-color-button);
+  color:var(--bg-color-button);
+  letter-spacing:0.25px;
+  transition:all 0.3s ease;
 }
 
 img{
   margin-right:6px;
+  width:28px;
+  height:28px;
+  
 }
 
-h2{
+header h2{
   text-transform: uppercase;
+  color:#242424;
+  /*
+  writing-mode: vertical-rl;
+text-orientation: mixed;
+*/
+}
+@media only screen and (min-width:300px) and (max-width:600px){
+  .web-navbar{
+    display:none;
+  }
+  .mobile-navbar{
+    display:block;
+  }
+  h2{
+    font-size:1.2rem;
+  }
 }
 </style>

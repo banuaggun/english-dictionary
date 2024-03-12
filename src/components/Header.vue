98078beded45a03ec7689dbdc5394cab2467e9d7
banuaggun <banuaggun@outlook.com>
2024-03-12 23:41:50 +0300

    sidebar view is created.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,25 +1,28 @@
 <template>
   <header>
     <div class="web-navbar">
-      <div class="row-1">
-        <img src="../assets/icons/logo.svg" alt="dictionary logo" />
-      <h2>English dictionary</h2>
-    </div>
-    <div class="social-media">
-      <div class="social-media-portfolio">
-        <a href="https://github.com/banuaggun" target="_blank">
-          <span>GitHub</span>
-        </a>
-        <a href="https://portfolio-banuaggun.vercel.app/" target="_blank">
-          <span>Portfolio</span>
-        </a>
+      <div class="web-navbar-row-2">
+        <div class="social-media">
+          <div class="social-media-portfolio">
+            <a href="https://github.com/banuaggun" target="_blank">
+              <i class="fa-brands fa-github"></i>
+            </a>
+            <a href="https://portfolio-banuaggun.vercel.app/" target="_blank">
+              <img src="../assets/icons/portfolio.png" />
+            </a>
+          </div>
+        </div>
       </div>
+      <div class="web-navbar-row-1">
+        <img src="../assets/icons/logo.svg" alt="dictionary logo" />
+        <h2>English Dictionary</h2>      
     </div>
+    
     </div>
      <div class="mobile-navbar">
       <div class="mobile-navbar-row-1">
         <img src="../assets/icons/logo.svg" alt="dictionary logo" />
-      <h2>English dictionary</h2>
+      <h3>English dictionary</h3>
     </div>
     <div class="mobile-navbar-row-2">
       <div class="social-media-portfolio">
@@ -36,6 +39,52 @@
 </template>
 
 <style scoped>
+header .web-navbar{
+  border:1px solid red;
+  width:80px;
+  height:100vh;
+  position:absolute;
+  top:0px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+  writing-mode: vertical-lr;
+  transform:rotate(180deg);
+}
+.web-navbar-row-1{
+  border:1px solid blue; 
+  display:flex;
+  align-items: center;
+}
+.web-navbar-row-1 img{
+  position: absolute;
+  bottom:40px;
+  border:1px solid black;
+  width:44px;
+  height:44px;
+}
+.web-navbar-row-1 h2{
+  bottom:100px;
+  position:absolute;
+}
+
+.web-navbar-row-2{
+  border:1px solid black;
+  position: absolute;
+  top:20px;
+}
+.web-navbar-row-2 img{
+  background-color:red;
+  border:1px solid red;
+  width:44px;
+  height:44px;
+  
+}
+i{
+  font-size:2rem;
+  transform:rotate(180deg);
+}
 header .mobile-navbar {
   border:1px solid green;
   display:flex;
@@ -80,6 +129,7 @@ img{
   
 }
 
+header h3,
 header h2{
   text-transform: uppercase;
   color:#242424;
@@ -90,13 +140,19 @@ text-orientation: mixed;
 }
 @media only screen and (min-width:300px) and (max-width:600px){
   .web-navbar{
-    display:none;
+    display:none !important;
   }
   .mobile-navbar{
     display:block;
   }
-  h2{
-    font-size:1.2rem;
-  }
 }
+
+ @media only screen and (min-width:601px) and (max-width:1920px){
+   .web-navbar{
+     display:block;
+   }
+   .mobile-navbar{
+     display:none !important;
+   }
+ }
 </style>
